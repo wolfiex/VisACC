7999f0dfb22f77bdf7f0c0b8972fc72f40c25f76
Daniel Ellis <daniel.ellis@york.ac.uk>
2017-02-16 21:34:41 +0000

    updates

--- TimeSpiral/readnc.js
+++ TimeSpiral/readnc.js
@@ -1,148 +1,173 @@
-
 ///classes and defns
 class nc2D {
-  constructor(name, width, arr_type) { //Float32Array
+  constructor(name, width, arr_type) {
+    //Float32Array
     this.width = dims[width];
     this.arr_type = arr_type;
     this.data = new arr_type(reader.getDataVariable(name));
   }
-};
-
-nc2D.prototype.row = function (index) {
-   var start = index*this.width;
-   return new this.arr_type(this.data.slice(start,start+this.width))
 }
 
-var print = (d) => console.log(d);
+nc2D.prototype.row = function(index) {
+  var start = index * this.width;
+  return new this.arr_type(this.data.slice(start, start + this.width));
+};
+
+var print = d => console.log(d);
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-var test = []
-
-
+var test = [];
 
 ///file read
-var reader,reader_url,dims, file;
+var reader, reader_url, dims, file;
 // read browser, adjust from there try except
 
-if(window.location.hash === ''){
+if (window.location.hash === "") {
   // Get the modal
-  closeNav()
-  var modal = document.getElementById('myModal');
+  closeNav();
+  var modal = document.getElementById("myModal");
   modal.style.display = "block";
+} else {
+  document.getElementById("myModal").remove();
+  file = window.location.hash.replace("#", "");
+  try {
+    const fs = require("fs");
+    const data = fs.readFileSync(
+      __dirname.match(/(.*\/)/)[1] + "netcdf_results/" + file + ".nc"
+    );
+    reader = new netcdfjs(data);
+    ncparse(reader);
+  } catch (err) {
+    console.log("switching to browser mode", err);
+    var urlpath = document.URL.match(/(.*\/).*\//)[1] +
+      "netcdf_results/" +
+      file +
+      ".nc";
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", urlpath, true);
+    oReq.responseType = "blob";
 
- } else {
-   document.getElementById('myModal').remove();
-   file = window.location.hash.replace('#','');
-      try{
-        const fs = require('fs');
-        const data = fs.readFileSync(__dirname+'/'+file+'.nc');
-        reader = new netcdfjs(data);
+    oReq.onload = function(oEvent) {
+      var blob = oReq.response;
+      reader_url = new FileReader();
+      reader_url.onload = function(e) {
+        reader = new netcdfjs(this.result);
         ncparse(reader);
-        }catch (err) {
-        console.log('switching to browser mode',err)
-        var urlpath = document.URL + file + ".nc"
-        var oReq = new XMLHttpRequest();
-        oReq.open("GET", urlpath, true);
-        oReq.responseType = "blob";
-
-        oReq.onload = function(oEvent) {
-          var blob = oReq.response;
-          reader_url = new FileReader();
-          reader_url.onload = function(e) {
-               reader = new netcdfjs(this.result);
-               ncparse(reader);
-               (function(){draw()})()
-              }
-          reader_url.readAsArrayBuffer(blob);
-        };
-        oReq.send(); //start process
-      }
-
-
+        (function() {
+          draw();
+        })();
+      };
+      reader_url.readAsArrayBuffer(blob);
+    };
+    oReq.send(); //start process
+  }
 }
 
+function ncparse(reader) {
+  //all parts we require from the netcdf file
+  reader.dimensions.forEach(
+    function(d) {
+      dims[d.name] = d.size;
+    },
+    dims = {}
+  );
+  const concentration = new nc2D("concentration", "specs", Float32Array);
+  const flux = new nc2D("edge-length", "fluxes", Float32Array);
+  const dict = JSON.parse(reader.getDataVariable("nodes").join(""));
+  const combine = JSON.parse(reader.getDataVariable("combinations").join(""));
+  const tar = reader.getDataVariable("target");
+  const src = reader.getDataVariable("source");
+  const time = reader.getDataVariable("timeseconds");
 
- function ncparse (reader){
-                //all parts we require from the netcdf file
-                reader.dimensions.forEach(function(d){dims[d.name] = d.size},dims={});
-                const concentration = new nc2D('concentration','specs',Float32Array);
-                const flux = new nc2D('edge-length','fluxes',Float32Array);
-                const dict = JSON.parse(reader.getDataVariable('nodes').join(''));
-                const combine = JSON.parse(reader.getDataVariable('combinations').join(''));
-                const tar = reader.getDataVariable('target');
-                const src = reader.getDataVariable('source');
-                const time = reader.getDataVariable('timeseconds');
+  // creates reverse dictionary rdict below
+  var rdict, nodes, i;
+  Object.keys(dict).forEach(
+    function(d) {
+      var i = dict[d];
+      rdict[i] = d;
+    },
+    rdict = {},
+    nodes = []
+  );
 
-                  // creates reverse dictionary rdict below
-                 var rdict,nodes,i;
-                  Object.keys(dict).forEach(function(d){var i = dict[d];rdict[i]=d;}, rdict={}, nodes=[]);
+  window.nodes = [];
+  for (i = 0; i < combine.length; i++) {
+    window.nodes.push({
+      names: rdict[i],
+      id: i,
+      x: 2 * (0.5 - Math.random()),
+      y: 2 * (0.5 - Math.random()),
+      z: 0.5 - Math.random()
+    });
+  }
 
-                window.nodes =[];for (i = 0; i < combine.length; i++) { window.nodes.push({"names":rdict[i],"id":i,"x":2*(0.5-Math.random()),
-                  "y":2*(0.5-Math.random()),"z":0.5-Math.random()}) };
+  const formatTime = d3.timeFormat("%b %d, %H:%M");
+  //time => datetime
+  const datetime = time.map(function(e) {
+    var utcSeconds = e;
+    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    d.setUTCSeconds(utcSeconds);
+    return d;
+  });
 
-                const formatTime = d3.timeFormat("%b %d, %H:%M");
-                  //time => datetime
-                const datetime=time.map(function(e){
-                      var utcSeconds = e;var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-                      d.setUTCSeconds(utcSeconds);
-                      return d });
+  window.ncdata = {
+    concentration,
+    flux,
+    dict,
+    rdict,
+    combine,
+    tar,
+    src,
+    formatTime,
+    datetime,
+    dims
+  };
 
+  var select = document.getElementById("end");
+  var select1 = document.getElementById("start");
 
-                window.ncdata ={concentration,flux,dict,rdict,combine,tar,src,formatTime,datetime,dims};
+  datetime.forEach((d, i) => {
+    var opt = document.createElement("option");
+    opt.value = i;
+    opt.innerHTML = formatTime(d);
+    select.appendChild(opt);
 
-                var select = document.getElementById('end');
-                var select1 = document.getElementById('start');
-
-                datetime.forEach((d,i)=> {
-
-                var opt = document.createElement('option');
-                opt.value = i;
-                opt.innerHTML = formatTime(d);
-                select.appendChild(opt);
-
-                var opt1 = document.createElement('option');
-                opt1.value = i;
-                opt1.innerHTML = formatTime(d);
-                select1.appendChild(opt1);
-
-
-
-                });
-
-
-
-                /*
+    var opt1 = document.createElement("option");
+    opt1.value = i;
+    opt1.innerHTML = formatTime(d);
+    select1.appendChild(opt1);
+  });
+  /*
                   .data(time).enter()
                   .append('option')
                     .text(function (d) { return formatTime(datetime[d]); });
 */
-
-
 }
-
 
 //get smiles strings and species data
 
-window.csvdata=[];
-window.N=[] ;
-window.C=[];
+window.csvdata = [];
+window.N = [];
+window.C = [];
 
-var inorganics= 'O,O3,O1D,O2,OH,NO,NO2,NO2N2O5,H2O2,HO2,HO2NO2,HONO,HNO3,CO,SO2,SO3,NA'.split(',')
-inorganics.forEach(function(d){
-  if (Object.keys(ncdata.dict).indexOf(d) >= 0 ) {
-         if (/^(.*[N].*)$/.test(d)) window.N.push(d);
-         if (/^(.*[Cc].*)$/.test(d)) window.C.push(d);
+var inorganics = "O,O3,O1D,O2,OH,NO,NO2,NO2N2O5,H2O2,HO2,HO2NO2,HONO,HNO3,CO,SO2,SO3,NA".split(
+  ","
+);
+inorganics.forEach(function(d) {
+  if (Object.keys(ncdata.dict).indexOf(d) >= 0) {
+    if (/^(.*[N].*)$/.test(d)) window.N.push(d);
+    if (/^(.*[Cc].*)$/.test(d)) window.C.push(d);
+  }
+});
+
+d3.csv("./fullmcmspecs.csv", function(error, csv) {
+  window.csvdata = csv;
+  for (var i = 0; i < csv.length; i++) {
+    var j = csv[i];
+    if (Object.keys(ncdata.dict).indexOf(j.item) >= 0) {
+      if (/^(.*[N].*)$/.test(j.smiles)) window.N.push(j.item);
+      if (/^(.*[Cc].*)$/.test(j.smiles)) window.C.push(j.item);
     }
-  });
-
-d3.csv("./fullmcmspecs.csv", function(error, csv) {window.csvdata=csv;
-  for (var i = 0; i < csv.length; i++){
-     var j = csv[i];
-     if (Object.keys(ncdata.dict).indexOf(j.item) >= 0 ) {
-         if (/^(.*[N].*)$/.test(j.smiles)) window.N.push(j.item);
-         if (/^(.*[Cc].*)$/.test(j.smiles)) window.C.push(j.item);
-    }
-  };
-
+  }
 });
--- revamp/main.js
+++ revamp/main.js
@@ -1,89 +1,99 @@
-
-
-const electron = require('electron');
-const cp = require('child_process');
+const electron = require("electron");
+const cp = require("child_process");
 const ipc = electron.ipcMain;
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
-const remote  = electron.remote;
+const app = electron.app; // Module to control application life.
+const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
+const remote = electron.remote;
 const Menu = electron.Menu;
 const MenuItem = electron.MenuItem;
 const webContents = electron.webContents;
 //ipc.send('toggle-prefs','la')
-
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != "darwin") {
+    app.quit();
+  }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
-    //sync init con files
-    var child = cp.exec('bash ./src/updateics.sh', function (error, stdout, stderr) {  console.log('updated ics');  child.stdout.pipe(process.stdout);});
-    cp.exec('touch heatmap.html && touch locations.json && rm heatmap.html && rm locations.json');
-    console.log('add datafile to read from web, iff this older, update');
+app.on("ready", function() {
+  //sync init con files
+  var child = cp.exec("bash ./src/updateics.sh", function(
+    error,
+    stdout,
+    stderr
+  ) {
+    console.log("updated ics");
+    child.stdout.pipe(process.stdout);
+  });
+  cp.exec(
+    "touch heatmap.html && touch locations.json && rm heatmap.html && rm locations.json"
+  );
+  console.log("add datafile to read from web, iff this older, update");
 
-    //var execSync = require('exec-sync');
-    //execSync('bash ./updateics.sh') ;
-    //require('child_process').exec('python celulas.py', function (error, stdout, stderr) {    child.stdout.pipe(process.stdout);});
+  //var execSync = require('exec-sync');
+  //execSync('bash ./updateics.sh') ;
+  //require('child_process').exec('python celulas.py', function (error, stdout, stderr) {    child.stdout.pipe(process.stdout);});
 
+  // Create the browser window.
+  const myLocation = "file://" + __dirname;
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 800,
+    resizable: true,
+    title: "Dan Ellis 2016",
+    show: true
+  });
+  mainWindow.openDevTools(); // and load the index.html of the app.
+  mainWindow.loadURL(myLocation + "/index.html");
 
-    // Create the browser window.
-    const myLocation = 'file://' + __dirname
-    mainWindow = new BrowserWindow({width:900, height: 800,resizable: true,title:'Dan Ellis 2016' ,
-    show:true
-      });
-    mainWindow.openDevTools();    // and load the index.html of the app.
-    mainWindow.loadURL( myLocation + '/index.html');
+  splash = new BrowserWindow({
+    width: 800,
+    height: 450,
+    resizable: false,
+    title: "VisACC 2016",
+    show: false
+  });
+  splash.loadURL(myLocation + "/hexbin.html");
+  splash.openDevTools();
 
-
-    splash = new BrowserWindow({width:800, height: 450,resizable: false ,title:'VisACC 2016' ,
-    show:false      });
-    splash.loadURL( myLocation + '/hexbin.html');
-    splash.openDevTools();
-
-
-    var prefsWindow = new BrowserWindow({
-        width: 990,
-        height: 140,
-        show: false,
-        title: 'This is where the magic happens'
-      });
-
-
-    prefsWindow.loadURL( myLocation + '/controls.html');
-    prefsWindow.openDevTools();
-
-
-
-
-
-    ipc.on('forwarder',(event,arg)=> {
-    console.log(eval(arg[0]));
-    eval(arg[0]).webContents.send('data' , {name:arg[1],values:arg[2]});
-    //can send directly.
-
-    ipc.on('decimate',(event,arg)=> {app.quit()});
-
-    ipc.on('closewindow',(event,arg)=> {eval(arg[0]).close()});
-
-    ipc.on('show',(event,arg)=> {eval(arg[0]).show()});
-
-
+  var prefsWindow = new BrowserWindow({
+    width: 990,
+    height: 140,
+    show: false,
+    title: "This is where the magic happens"
   });
 
+  prefsWindow.loadURL(myLocation + "/controls.html");
+  prefsWindow.openDevTools();
 
-/*
+  ipc.on("forwarder", (event, arg) => {
+    console.log(eval(arg[0]));
+    eval(arg[0]).webContents.send("data", { name: arg[1], values: arg[2] });
+    //can send directly.
+
+    ipc.on("decimate", (event, arg) => {
+      app.quit();
+    });
+
+    ipc.on("closewindow", (event, arg) => {
+      eval(arg[0]).close();
+    });
+
+    ipc.on("show", (event, arg) => {
+      eval(arg[0]).show();
+    });
+  });
+
+  /*
     ipc.on('toggle-prefs', (event,arg)=> {
     if (prefsWindow.isVisible())
       prefsWindow.hide()
@@ -97,8 +107,8 @@ app.on('ready', function() {
 
 */
 
-///menu
-/*
+  ///menu
+  /*
       var fileMenu = new Menu();
       fileMenu.append(new MenuItem({ label: "Save Image", accelerator: "Ctrl+s", click: function() {
           fileMenu.clear(); //editMenu.append(new MenuItem({ label: "Dummy2", accelerator: "Ctrl+P", click: function() {
@@ -126,27 +136,23 @@ saveMenu.append(new MenuItem({ label: "save", accelerator: "Ctrl+s",  click:func
 
 */
 
-      function getWindow(windowName) {
-        for (var i = 0; i < windowArray.length; i++) {
-          if (windowArray[i].name == windowName) {
-            return windowArray[i].window;
-          }
-        }
-        return null;
+  function getWindow(windowName) {
+    for (var i = 0; i < windowArray.length; i++) {
+      if (windowArray[i].name == windowName) {
+        return windowArray[i].window;
       }
+    }
+    return null;
+  }
 
-
-
-
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-        prefsWindow=null;
-        app.quit();
-// make a function to trigger
-    });
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+    prefsWindow = null;
+    app.quit();
+    // make a function to trigger
+  });
 });
